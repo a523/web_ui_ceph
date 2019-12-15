e1809adea04a19feb9198ee0e20d58c6b590820c
xin <chjgreen@qq.com>
2019-12-15 18:21:15 +0800

    匹配后端数据

--- src/store/getters.js
+++ src/store/getters.js
@@ -3,7 +3,7 @@ const getters = {
   device: state => state.app.device,
   token: state => state.user.token,
   avatar: state => state.user.avatar,
-  name: state => state.user.name,
+  name: state => state.user.username,
   roles: state => state.user.roles,
   permission_routes: state => state.permission.routes
 }
